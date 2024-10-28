ef62bc3105e07ab062e0eedcb5b6971818fee5e5
Tariq Lashley <tariqlash@gmail.com>
2024-10-28 15:02:02 -0400

    Added Pagination, select rows and a delete button to data table.

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -15,6 +15,18 @@ const data: Payment[] = [
         status: "pending",
         email: "m@example.com",
       },
+      {
+        id: "728ed52f",
+        amount: 45,
+        status: "success",
+        email: "a@example.com",
+      },
+      {
+        id: "728ed52f",
+        amount: 88,
+        status: "success",
+        email: "b@example.com",
+      },
 ]
 
 const AccountsPage = () => {
@@ -23,8 +35,8 @@ const AccountsPage = () => {
 
     return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-        <Card className="border-none drop-shadow-sm">
-            <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+        <Card className="border-none drop-shadow-sm p-0">
+            <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between p-3">
             <CardTitle className="text-xl line-clamp-1">
             Accounts Page
             </CardTitle>
@@ -33,7 +45,13 @@ const AccountsPage = () => {
                 Add New
             </Button>
             </CardHeader>
-            <DataTable columns={columns} data={data} />
+            <DataTable 
+            columns={columns} 
+            data={data}
+            filterKey="email"
+            onDelete={() => {}}
+            disabled={false}
+            />
         </Card>
     </div>
   )

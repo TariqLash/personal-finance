9fa880bccaa2f35b27c60693edcba9447c18040d
Tariq Lashley <tariqlash@gmail.com>
2024-11-03 22:39:23 -0500

    finished most of the graphs on dashboard

--- README.md
+++ README.md
@@ -40,3 +40,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 6:41
 8:02
 8:54
+9:31
--- app/api/[[...route]]/summary.tsx
+++ app/api/[[...route]]/summary.tsx
@@ -82,8 +82,8 @@ const app = new Hono()
             );
             const [lastPeriod] = await fetchFinancialData(
                 auth.userId,
-                startDate,
-                endDate,
+                lastPeriodStart,
+                lastPeriodEnd,
             );
 
             const incomeChange = calculatePercentageChange(
@@ -153,7 +153,7 @@ const app = new Hono()
                 .select({
                     date: transactions.date,
                     income: sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
-                    expenses: sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+                    expenses: sql`SUM(CASE WHEN (${transactions.amount}) < 0 THEN ABS(${transactions.amount}) ELSE 0 END)`.mapWith(Number),
                 })
                 .from(transactions)
                 .innerJoin(
